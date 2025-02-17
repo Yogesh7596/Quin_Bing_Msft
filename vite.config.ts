c69903284af8f079dfc0ba8380d7cd16a1f201fc
Yogesh7596 <yogesh.g@affine.ai>
2025-02-17 14:23:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,23 +17,10 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: true,
     rollupOptions: {
       output: {
-        manualChunks: (id) => {
-          if (id.includes('@fluentui/react-icons')) {
-            return 'fluentui-icons';
-          } else if (id.includes('@fluentui/react')) {
-            return 'fluentui-react';
-          } else if (id.includes('node_modules')) {
-            return 'vendor';
-          } else if  (id.includes('@fluentui/react-window-provider')) {
-            return 'fluent-ui'; 
-          }
-        },
-      },
-    },
-  },
+        manualChunks: () => 'everything.js' // Forces all into one bundle
+      }
+    }
+  }
 });
